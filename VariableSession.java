2f241d7f581cc4e8195fce73d64113d1cd0ba6ff
AthulSachu <athulsachu80@gmail.com>
2024-09-26 23:07:05 +0530

    Variable and datatype

---
+++ VariableSession.java
@@ -0,0 +1,27 @@
+public class VariableSession {
+    public static void main(String[] args) {
+        byte numberOfWheels = 4;          
+        byte buildingFloors = 50;         
+        byte ageOfChild = 7;              
+        byte maxSpeedLimit = 120;         
+        byte temperatureInCelsius = -5;   
+        byte daysInWeek = 7;              
+        byte playerLives = 3;            
+        byte roomsInApartment = 7;  
+        System.out.println(roomsInApartment);
+        roomsInApartment = 3;
+        System.out.println(roomsInApartment);
+
+        short flightAltitude = 10000;         
+        short averageRainfall = 1200;         
+        short phoneBatteryCapacity = 4500;    
+        short maxFileSize = 32767;            
+        short trainSpeed = 160;               
+        short classroomCapacity = 45;         
+        short averageHeartRate = 72;         
+        short depthOfOcean = 11000;
+        System.out.println(trainSpeed  );
+        trainSpeed = 180;
+        System.out.println(trainSpeed);             
+    }
+}
