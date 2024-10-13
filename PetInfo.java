02b8ee948cf6bfa18ca16b03d0bbf9e1595b8a69
AthulSachu <athulsachu80@gmail.com>
2024-10-13 17:07:35 +0530

    Class and Object Example

---
+++ PetInfo.java
@@ -0,0 +1,161 @@
+public class PetInfo {
+	public static void main(String args[]) {
+		Pet pet = new Pet();
+		
+		pet.name = "Bella";
+		pet.age = 3;
+    	pet.type =	"Dog";
+    	pet.breed =	"Golden Retriever";
+    	pet.weight = 24.0f;
+
+    	System.out.println(pet.name);
+		System.out.println(pet.age);
+		System.out.println(pet.type);
+		System.out.println(pet.breed);
+		System.out.println(pet.weight);
+
+		System.out.println("<-------------------------------------->");
+		
+		Pet pet1 = new Pet();
+
+		pet1.name = "Linda";
+		pet1.age = 2;
+    	pet1.type =	"Cat";
+    	pet1.breed = "Persian";
+    	pet1.weight = 4.5f;
+
+    	System.out.println(pet1.name);
+		System.out.println(pet1.age);
+		System.out.println(pet1.type);
+		System.out.println(pet1.breed);
+		System.out.println(pet1.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet2 = new Pet();
+
+		pet2.name = "Rocky";
+		pet2.age = 4;
+    	pet2.type =	"Dog";
+    	pet2.breed = "Bulldog";
+    	pet2.weight = 20.5f;
+
+    	System.out.println(pet2.name);
+		System.out.println(pet2.age);
+		System.out.println(pet2.type);
+		System.out.println(pet2.breed);
+		System.out.println(pet2.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet3 = new Pet();
+
+		pet3.name = "Simba";
+		pet3.age = 2;
+    	pet3.type =	"Cat";
+    	pet3.breed = "Maine Coon";
+    	pet3.weight = 6.0f;
+
+    	System.out.println(pet3.name);
+		System.out.println(pet3.age);
+		System.out.println(pet3.type);
+		System.out.println(pet3.breed);
+		System.out.println(pet3.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet4 = new Pet();
+
+		pet4.name = "Charlie";
+		pet4.age = 6;
+    	pet4.type =	"Dog";
+    	pet4.breed = "German Shepherd";
+    	pet4.weight = 30.0f;
+
+    	System.out.println(pet4.name);
+		System.out.println(pet4.age);
+		System.out.println(pet4.type);
+		System.out.println(pet4.breed);
+		System.out.println(pet4.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet5 = new Pet();
+
+		pet5.name = "Zindra";
+		pet5.age = 3;
+    	pet5.type =	"Cat";
+    	pet5.breed = "Korat";
+    	pet5.weight = 5.0f;
+
+    	System.out.println(pet5.name);
+		System.out.println(pet5.age);
+		System.out.println(pet5.type);
+		System.out.println(pet5.breed);
+		System.out.println(pet5.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet6 = new Pet();
+
+		pet6.name = "Shadow";
+		pet6.age = 5;
+    	pet6.type =	"Dog";
+    	pet6.breed = "Pomeranian";
+    	pet6.weight = 8.0f;
+
+    	System.out.println(pet6.name);
+		System.out.println(pet6.age);
+		System.out.println(pet6.type);
+		System.out.println(pet6.breed);
+		System.out.println(pet6.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet7 = new Pet();
+
+		pet7.name = "Shaw";
+		pet7.age = 4;
+    	pet7.type =	"Dog";
+    	pet7.breed = "Beagle";
+    	pet7.weight = 9.0f;
+
+    	System.out.println(pet7.name);
+		System.out.println(pet7.age);
+		System.out.println(pet7.type);
+		System.out.println(pet7.breed);
+		System.out.println(pet7.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet8 = new Pet();
+
+		pet8.name = "Rooney";
+		pet8.age = 8;
+    	pet8.type =	"Dog";
+    	pet8.breed = "Rottweiler";
+    	pet8.weight = 29.5f;
+
+    	System.out.println(pet8.name);
+		System.out.println(pet8.age);
+		System.out.println(pet8.type);
+		System.out.println(pet8.breed);
+		System.out.println(pet8.weight);
+
+		System.out.println("<-------------------------------------->");
+
+		Pet pet9 = new Pet();
+
+		pet9.name = "Lucky";
+		pet9.age = 5;
+    	pet9.type =	"Cat";
+    	pet9.breed = "British Shorthair";
+    	pet9.weight = 5.5f;
+
+    	System.out.println(pet9.name);
+		System.out.println(pet9.age);
+		System.out.println(pet9.type);
+		System.out.println(pet9.breed);
+		System.out.println(pet9.weight);
+	}
+}
