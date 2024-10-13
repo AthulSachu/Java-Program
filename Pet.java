99124e48d372211ee0204551157a7d6aa1c1a15d
AthulSachu <athulsachu80@gmail.com>
2024-10-13 17:07:05 +0530

    State and Behaviour

---
+++ Pet.java
@@ -0,0 +1,25 @@
+public class Pet {
+	//State
+	String name;
+	int age;
+    String type;
+    String breed;
+    float weight;
+
+    //Behaviour
+    public void play() {
+
+    }
+
+    public void takeForWalk() {
+        System.out.println(name + " is playing.");
+    }
+
+    public void makeSound() {
+        
+    }
+
+    public void getVeterinaryCheckup() {
+
+    }
+}
