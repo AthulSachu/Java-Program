566e6f2cd9c16775e405711cb6ca6d8f36b44097
AthulSachu <athulsachu80@gmail.com>
2024-11-07 01:33:20 +0530

    Method Overriding Example

---
+++ gstcalculation/Gst.java
@@ -0,0 +1,26 @@
+package gstcalculation;
+
+public class Gst {
+	String stateName;
+	String product;
+	double amount;
+	
+	public Gst(String stateName, String product, double amount) {
+		this.stateName = stateName;
+		this.product = product;
+		this.amount = amount;
+	}
+	
+	public double calculateGST() {
+        double gstRate = 18.0; 
+        double gst = amount * gstRate / 100;
+        System.out.println("GST " + gst);
+        return gst;
+    }
+	
+	public void displayInfo() {
+        System.out.println("State: " + stateName);
+        System.out.println("Product: " + product);
+        System.out.println("Amount: " + amount);
+    }
+}
