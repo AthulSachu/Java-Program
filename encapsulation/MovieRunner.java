4a2162a9bcf5f4d124787ce9ce5d19ae9278b648
AthulSachu <athulsachu80@gmail.com>
2024-11-13 01:15:11 +0530

    Encapsulation Example

--- encapsulation/MovieRunner.java
+++ encapsulation/MovieRunner.java
@@ -29,5 +29,27 @@ public class MovieRunner {
 		boolean inTheaters = movie.getInTheaters();
 		
 		System.out.println("\nIs in Theaters: "+inTheaters);
+		
+		Movies movie1 = new Movies("Drishyam", "Jeethu Joseph", "Crime Thriller", 2013, "Malayalam", 160, "Aashirvad Cinemas", 0.5, 75.0, 8.5, false, "Mohanlal", "Meena", "Anil Johnson", "Sujith Vaassudev", "Jeethu Joseph", "Mohanlal", "Maxlab Cinemas", true, "India");
+		
+		System.out.println("");
+		
+		movie1.printDetails();
+
+		movie1.setBudget(0.8);
+		double up1MovieBudget = movie.getBudget();
+		System.out.println("\nUpdate in Movie Budget: " + up1MovieBudget);
+
+		movie1.setBoxOffice(80.0); 
+		double up1BoxOffice = movie.getBoxOffice();
+		System.out.println("\nNew Box Office Collection: " + up1BoxOffice);
+
+		movie1.setReleaseYear(2015);
+		int releaseYear1 = movie.getReleaseYear();
+		System.out.println("\nRelease Year: " + releaseYear1);
+
+		movie1.setInTheaters(false);  
+		boolean inTheaters1 = movie.getInTheaters();
+		System.out.println("\nIs in Theaters: " + inTheaters1);
 	}
 }
